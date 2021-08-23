1b68e90645e1b61960700ed22d1e956a89ed7b3e
Håvard Farestveit <havard.farestveit@ntnu.no>
2021-08-23 15:37:06 +0200

    La til permissions på pledges

--- pledge/urls.py
+++ pledge/urls.py
@@ -1,9 +1,9 @@
-
 from django.urls import path
 from pledge import views
 
 urlpatterns = [
-    path('pledge', views.PledgeList.as_view()),
-    path('ok', views.PledgeUserList.as_view()),
+    path('pledge/', views.PledgeList.as_view()),
+    path('userpledge/user/', views.CreatePledgeUser.as_view()),
+    path('userpledge/user/<int:pk>', views.PledgeUserList.as_view()),
     path('userpledge/<int:pk>', views.PledgeUserDelete.as_view()),
 ]
