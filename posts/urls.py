4ff4ed43667dd6f8afad4a8e07bc78d6446cf919
Håvard Farestveit <havard.farestveit@ntnu.no>
2021-10-18 08:55:07 +0200

    Fikset urls for posts

--- posts/urls.py
+++ posts/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('posts/<int:pk>/', views.PostDetail.as_view()),
     path('posts/users/self/', views.MyPostList.as_view()),
     path('posts/team/<int:pk>/', views.TeamPostList.as_view()),
-    path('likes', views.Likes.as_view()),
-    path('liked/<int:pk>', views.Liked.as_view()),
-    path('likes/post/<int:pk>', views.LikeList.as_view()),
-    path('likes/<int:pk>', views.LikeDelete.as_view()),
-    path('comment', views.NewComment.as_view()),
-    path('comments/<int:pk>', views.CommentsDelete.as_view()),
-    path('comments/<int:pk>', views.CommentList.as_view())
+    path('likes/', views.Likes.as_view()),
+    path('liked/<int:pk>/', views.Liked.as_view()),
+    path('likes/post/<int:pk>/', views.LikeList.as_view()),
+    path('likes/<int:pk>/', views.LikeDelete.as_view()),
+    path('comment/', views.NewComment.as_view()),
+    path('comments/<int:pk>/', views.CommentsDelete.as_view()),
+    path('comments/<int:pk>/', views.CommentList.as_view())
 ]
