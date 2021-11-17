95a58987caddeebae9183efe22594081642f532c
Håvard Farestveit <havard.farestveit@ntnu.no>
2021-11-17 20:28:53 +0100

    Oppdaterte following av andre brukerer

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('followers/', views.Followers.as_view(), name="followers"),
     path('following/', views.Following.as_view(), name='following'),
     path('not-following/', views.NotFollowingList.as_view(), name='following'),
-    path('follow/<int:pk>/', views.follow, name='follow'),
+    path('follow/<int:pk>/', views.Follow.as_view(), name='follow'),
+    path('unfollow/<int:pk>/', views.Unfollow.as_view(), name='unfollow'),
 ]
